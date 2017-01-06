d144cdb37623187bc5a2230a9f4a6ed79a8f8cd0
TGills <tylerguillaume@live.com>
2017-01-06 11:13:13 -0600

    yes

---
+++ Weather/App_Code/WeatherUtilities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+/// <summary>
+/// Summary description for WeatherUtilities
+/// </summary>
+public class WeatherUtilities
+{
+    string conString;
+
+    public WeatherUtilities()
+    {
+        conString = WebConfigurationManager.ConnectionStrings["dbWeatherData"].ConnectionString;
+    }
+
+    public void addWeatherReport(WeatherReport wr)
+    {
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = conString;
+        SqlCommand cmd = new SqlCommand(
+            "Insert Into Reports (lat,lon,timesubmitted,facing) Values(@lat,@lon,@datesubmitted,@facing)"
+         );
+
+        cmd.Parameters.AddWithValue("lat", wr.Lat);
+        cmd.Parameters.AddWithValue("lon", wr.Lon);
+        cmd.Parameters.AddWithValue("facing", wr.Facing);
+        cmd.Parameters.AddWithValue("datesubmitted", wr.DateSubmitted);
+
+        cmd.Connection = con;
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+
+    }
+
+
+
+
+}
---
+++ Weather/WeatherReportViewer.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WeatherReportViewer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        float lat = Convert.ToSingle(txtLat.Text);
+        float lon = Convert.ToSingle(txtLon.Text);
+        float facing = Convert.ToSingle(txtFacing.Text);
+        
+        WeatherReport wr = 
+            new WeatherReport(-1, lat, lon, facing, DateTime.Now);
+
+        WeatherUtilities wu = new WeatherUtilities();
+        wu.addWeatherReport(wr);
+
+      
+
+    }
+}
